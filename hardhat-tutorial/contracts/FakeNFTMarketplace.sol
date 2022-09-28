8e3302defdb6edaa2abe561fb7731f67b7f16f77
ronkips <kipshillash@gmail.com>
2022-09-28 13:25:48 +0300

    changed the alchemy url to git quicknode endpoint

--- hardhat-tutorial/contracts/FakeNFTMarketplace.sol
+++ hardhat-tutorial/contracts/FakeNFTMarketplace.sol
@@ -7,11 +7,11 @@ pragma solidity ^0.8.9;
 contract FakeNFTMarketplace {
     /// @dev Mantain a mapping of fake TokenId to Owner address
     mapping(uint256 => address) public tokens;
-    /// @dev set the purchase price for each Fake NFT
+    // @dev set the purchase price for each Fake NFT
     uint256 nftPrice = 0.01 ether;
 
-    /// @dev purchase() accepts ETH and marks the owner of the given tokenId as the caller address
-    /// @param _tokenId - the fake NFT token Id to purchase
+    // @dev purchase() accepts ETH and marks the owner of the given tokenId as the caller address
+    // @param _tokenId - the fake NFT token Id to purchase
     function purchase(uint256 _tokenId) external payable {
         require(msg.value == nftPrice, "This NFT cost 0.01 ether");
         tokens[_tokenId] = msg.sender;
